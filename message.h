37c3446d7c9f77a777bbf76988a404904559e543
JimeRosello <jimena.rosello@gmail.com>
2016-10-21 11:56:35 -0300

    Update message.h

--- message.h
+++ message.h
@@ -7,7 +7,7 @@ class Message {
 
 	private:
 		string body;
-		int sender;
+		int receiver;
 
 	public:
 
@@ -15,16 +15,17 @@ class Message {
 		// pre: El identificador del remitente pasado como parametro
 		//      (sender) es valido
 		// post: Se creo un mensaje, con los atributos cuerpo (body)
-		//       y remitente (sender)
-		Message(string body, int sender);
+		//       y el celular que lo recibe (receiver)
+		Message(string body, int receiver);
 
 		// pre: -
 		// post: Devuelve el cuerpo del mensaje
 		string getBody();
 
 		// pre: -
-		// post: Devuelve el identificador del remitente del mensaje
-		int getSender();
+		// post: Devuelve el identificador del celular que recibe el
+		//       mensaje
+		int getReceiver();
 
 		// Destructor
 		// pre: -
