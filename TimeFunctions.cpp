9c7895745fb01670b9cb3820d03c0c6bf36c7ff7
joel-perez <joperezr@gmail.com>
2016-11-03 00:37:10 -0300

    Agrego la funcion getMinutesSinceMidnight que devuelve la hora actual expresada como cantidad de minutos desde las 00:00hs

---
+++ TimeFunctions.cpp
@@ -0,0 +1,9 @@
+#include <ctime>
+#include "TimeFunctions.h"
+
+unsigned int getMinutesSinceMidnight()
+{
+	time_t seconds;
+	seconds = time(NULL);
+	return (((seconds + hoursOffsetFromUTC * 3600) % (24 * 3600)) / 60);
+}
---
+++ TimeFunctions.h
@@ -0,0 +1,9 @@
+#ifndef TIMEFUNCTIONS_H_
+#define TIMEFUNCTIONS_H_
+
+//Post: devuelve la hora actual, expresada como cantidad de minutos desde las 00:00hs
+unsigned int getMinutesSinceMidnight();
+
+const int hoursOffsetFromUTC = -3;
+
+#endif /* TIMEFUNCTIONS_H_ */
