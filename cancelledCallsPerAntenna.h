54c07d70d00259c7edea905cd72b4c8ff02cc170
Pavlo Holota <pavlo.holota@gmail.com>
2016-12-11 16:41:59 -0300

    Update cancelledCallsPerAntenna.h

--- cancelledCallsPerAntenna.h
+++ cancelledCallsPerAntenna.h
@@ -2,40 +2,40 @@
 #define CANCELLEDCALLSPERANTENNA_H_
 
 class cancelledCallsPerAntenna {
-	private:
-		unsigned int antennaId;
-		unsigned int numberOfCalls;
+private:
+	unsigned int antennaId;
+	unsigned int numberOfCalls;
 
-	public:
+public:
 
-		/*
-		 * Constructor
-		 */
-		cancelledCallsPerAntenna();
+	/*
+	 * Constructor
+	 */
+	cancelledCallsPerAntenna();
 
-		/*
-		 * Pre: ---;
-		 * Post: Devuelve el identificador de la antena.
-		 */
-		unsigned int getAntennaId();
+	/*
+	 * Pre: ---;
+	 * Post: Devuelve el identificador de la antena.
+	 */
+	unsigned int getAntennaId();
 
-		/*
-		 * Pre: ---;
-		 * Post: Devuelve el numero de llamadas canceladas.
-		 */
-		unsigned int getNumberOfCancelledCalls();
+	/*
+	 * Pre: ---;
+	 * Post: Devuelve el numero de llamadas canceladas.
+	 */
+	unsigned int getNumberOfCancelledCalls();
 
-		/*
-		 * Pre: El numero de llamadas es valido.
-		 * Post: Cambia el numero de llamadas del objeto.
-		 */
-		void changeNumberOfCancelledCalls(unsigned int numberOfCancelledCalls);
+	/*
+	 * Pre: El numero de llamadas es valido.
+	 * Post: Cambia el numero de llamadas del objeto.
+	 */
+	void changeNumberOfCancelledCalls(unsigned int numberOfCancelledCalls);
 
-		/*
-		 * Pre: El id de la antena es valido.
-		 * Post: Cambia el id de la antena del objeto.
-		 */
-		void changeAntennaId(unsigned int antennaId);
+	/*
+	 * Pre: El id de la antena es valido.
+	 * Post: Cambia el id de la antena del objeto.
+	 */
+	void changeAntennaId(unsigned int antennaId);
 
 };
 
