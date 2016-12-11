42c26704c9f9fe040081cad729241c0ba0520d2d
Pavlo Holota <pavlo.holota@gmail.com>
2016-12-11 16:40:59 -0300

    Update Console.cpp

--- Console.cpp
+++ Console.cpp
@@ -1,19 +1,19 @@
 #include "Console.h"
 
-int RequestNumericInput(){
+int RequestNumericInput() {
 	unsigned int number = 0;
-	if(!(std::cin >> number)){
+	if (!(std::cin >> number)) {
 		std::cin.clear();
 		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 		std::cout << "Debe ingresar un numero valido." << std::endl;
-    }
+	}
 	return number;
 }
 
-std::string RequestTextLineInput(){
+std::string RequestTextLineInput() {
 	std::string textLine = "";
-    std::cin.clear();
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	std::cin.clear();
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 	std::getline(std::cin, textLine);
 	return textLine;
 }
